f4a88994fb86d3a41f27faff612bbda8b83cd8b5
Sinan Aumarah <sinan.aumarah@gmail.com>
2019-02-11 22:10:40 +1100

    example of how events should work

--- src/index.js
+++ src/index.js
@@ -1,55 +1,22 @@
-import {LitElement, html} from '@polymer/lit-element';
-import retargetEvents from 'react-shadow-dom-retarget-events';
 import React, { Component } from 'react';
 import * as ReactDOM from 'react-dom';
 import {StyleSheetManager} from "styled-components"
 
 import App from './app.js';
 
-// class MyWebComponent extends LitElement {
-//   constructor() {
-//     super();
-//     //this.attachShadow({mode: 'open'});
-//   }
-//
-//   getShadowDom() {
-//     return `<slot></slot>`;
-//   }
-//
-//   render() {
-//     const shadowDom = this.getShadowDom();
-//     const mountPoint = document.createElement('span');
-//     const styleContainer = document.createElement("div");
-//       ReactDOM.render(
-//           <StyleSheetManager target={styleContainer}>
-//             <App />
-//           </StyleSheetManager>, mountPoint);
-//
-//     this.shadowRoot.appendChild(styleContainer);
-//     this.shadowRoot.appendChild(mountPoint);
-//     retargetEvents(this.shadowRoot);
-//   }
-//
-//   connectedCallback() {
-//     this.render();
-//   }
-// }
+class MyWebComponent extends HTMLElement {
+    constructor() {
+        // Always call super first in constructor
+        super();
 
-//customElements.define('my-webcomponent', MyWebComponent);
+        // Add shadow root to component.
+        const shadow = this.attachShadow({ mode: 'open' });
+        const props = JSON.parse(this.getAttribute("appConfig"));
 
-
-//const container = document.createElement('app');
-//document.body.appendChild(container);
-
-// Add shadow root to component.
-const shadow = document.querySelector('my-webcomponent').createShadowRoot({ mode: 'open' });
-const styleContainer = document.createElement("div");
-const props = JSON.parse(document.querySelector('my-webcomponent').getAttribute("appConfig"));
-shadow.appendChild(styleContainer);
-
-// Select the web component, then the shadowRoot.
-const target = document.querySelector('my-webcomponent').shadowRoot;
-
-ReactDOM.render(<StyleSheetManager target={target}>
-    <App {...props}/>
-</StyleSheetManager>, target);
+        // Select the web component, then the shadowRoot
+        ReactDOM.render(<StyleSheetManager target={shadow}>
+            <App {...props}/>
+        </StyleSheetManager>, shadow);
+    }
+}
+customElements.define('my-webcomponent', MyWebComponent);
